ee79eee49daf997fea37ead8982ad98ffd0d895f
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-06-01 16:38:09 +0530

    Dynamic Programming: Bottom Up Approach for Rod Cutting

---
+++ CuttingRodBottomUp.py
@@ -0,0 +1,19 @@
+import random
+
+random.seed(100)
+
+price = [random.randint(1,100) for _ in range(100)]
+length = [i for i in range(1,101)]
+
+price_per_length = {key:value for key,value in zip(length, price)}
+
+def CutRod(length):
+    computed_lengths = [0]
+    for rod_length in range(1, length+1):
+        optimised_cost = float("-inf")
+        for sub_rod_length in range (1, rod_length+1):
+            optimised_cost = max(optimised_cost, price_per_length[sub_rod_length] + computed_lengths[rod_length - sub_rod_length])
+        computed_lengths.append(optimised_cost)
+    return computed_lengths[length]
+
+print CutRod(20)
