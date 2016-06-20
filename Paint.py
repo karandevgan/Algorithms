a3bf17a5165ff8b10b6f19791fb7248a9d3332de
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-06-20 18:24:06 +0530

    Paniter's Partition Problem: Optimizes

--- Paint.py
+++ Paint.py
@@ -4,7 +4,12 @@ class Solution:
     # @param B : integer
     # @param C : list of integers
     # @return an integer
-    def getRequiredPainters(self, C, max_length):
+    class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : list of integers
+    # @return an integer
+    def isPossible(self, C, max_length, num_of_painters):
         size = len(C)
         count = 1
         total = 0
@@ -13,7 +18,9 @@ class Solution:
             if total > max_length:
                 count += 1
                 total = C[i]
-        return count
+                if count > num_of_painters:
+                    return False
+        return True
 
     def partition(self, A, C):
         num_of_painters = A
@@ -21,12 +28,10 @@ class Solution:
         high = sum(C)
         while (low < high):
             mid = low + (high - low) / 2
-            required_painters = self.getRequiredPainters(C, mid)
-            if required_painters <= num_of_painters:
+            if self.isPossible(C, mid, num_of_painters):
                 high = mid
             else:
                 low = mid + 1
-
         return low
 
     def paint(self, A, B, C):
