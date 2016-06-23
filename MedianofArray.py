6976be7c7f9e73138e195c4a803316e7fb91ca4b
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-06-23 15:01:26 +0530

    Median of Sorted Array: Optimized

--- MedianofArray.py
+++ MedianofArray.py
@@ -19,60 +19,74 @@ class Solution:
     def findMedianOfTwo(self, numA, numB):
         return (numA + numB) / 2.0
 
-    def findMedianOfOneArray(self, Arr):
-        if len(Arr) % 2 == 0:
-            return (Arr[len(Arr) / 2] + Arr[len(Arr) / 2 - 1]) / 2.0
+    def findMedianOfOneArray(self, Arr, start, end):
+        # import pdb
+        # pdb.set_trace()
+        lenArr = end - start + 1
+        if lenArr % 2 == 0:
+            return (Arr[start + lenArr / 2] + Arr[start + lenArr / 2 - 1]) / 2.0
         else:
-            return Arr[len(Arr) / 2]
+            return Arr[start + lenArr / 2]
 
     def findMedianSortedArrays(self, A, B):
         # import pdb
         # pdb.set_trace()
         if len(A) == 0:
-            return self.findMedianOfOneArray(B)
+            return self.findMedianOfOneArray(B, 0, len(B)-1)
         if len(B) == 0:
-            return self.findMedianOfOneArray(A)
+            return self.findMedianOfOneArray(A, 0, len(A)-1)
 
         # Assume that len(A) <= len(B)
         # Swap the arrays if len(A) > len(B)
         if len(A) > len(B):
             A, B = B, A
 
-        if len(A) == 1:
-            if len(B) % 2 == 0:
-                return self.findMedianOfThree(A[0], B[len(B) / 2 - 1], B[len(B) / 2])
-            elif len(B) > 2:
-                medB = B[len(B) / 2]
-                if A[0] >= B[len(B) / 2 - 1] and A[0] <= B[len(B) / 2 + 1]:
-                    return self.findMedianOfTwo(A[0], medB)
-                elif A[0] < medB:
-                    return self.findMedianOfTwo(B[len(B) / 2 - 1], medB)
+        startA = 0
+        endA = len(A) - 1
+        startB = 0
+        endB = len(B) - 1
+        while True:
+            lenA = endA - startA + 1
+            lenB = endB - startB + 1
+
+            if lenA == 1:
+                if lenB % 2 == 0:
+                    return self.findMedianOfThree(A[startA], B[startB + lenB / 2 - 1], B[startB + lenB / 2])
+                elif lenB > 2:
+                    medB = B[startB + lenB / 2]
+                    if A[startA] >= B[startB + lenB / 2 - 1] and A[startA] <= B[startB + lenB / 2 + 1]:
+                        return self.findMedianOfTwo(A[startA], medB)
+                    elif A[startA] < medB:
+                        return self.findMedianOfTwo(B[startB + lenB / 2 - 1], medB)
+                    else:
+                        return self.findMedianOfTwo(medB, B[startB + lenB / 2 + 1])
                 else:
-                    return self.findMedianOfTwo(medB, B[len(B) / 2 + 1])
+                    return self.findMedianOfTwo(A[startA], B[startB])
+
+            if lenA == 2 and lenB % 2 == 0:
+                if A[startA] >= B[startB + lenB / 2 - 1] and A[endA] <= B[startB + lenB / 2]:
+                    return self.findMedianOfTwo(A[startA], A[endA])
+                elif A[startA] < B[startB + lenB / 2 - 1] and A[endA] > B[startB + lenB / 2]:
+                    return self.findMedianOfTwo(B[startB + lenB / 2 - 1], B[startB + lenB / 2])
+
+            medA = self.findMedianOfOneArray(A, startA, endA)
+            medB = self.findMedianOfOneArray(B, startB, endB)
+            if medA == medB:
+                return medA
+
+            maxDiscardable = lenA / 2 - 1
+            if (lenA % 2 != 0 or lenA == 2):
+                maxDiscardable = lenA / 2
+            if medA < medB:
+                startA = startA + maxDiscardable
+                endB = endB - maxDiscardable
+                # return self.findMedianSortedArrays(A[startA + maxDiscardable:endA + 1], B[startB:endB - maxDiscardable + 1])
             else:
-                return self.findMedianOfTwo(A[0], B[0])
+                endA = endA - maxDiscardable
+                startB = startB + maxDiscardable
 
-        if len(A) == 2 and len(B) % 2 == 0:
-            if A[0] >= B[len(B) / 2 - 1] and A[1] <= B[len(B) / 2]:
-                return self.findMedianOfTwo(A[0], A[1])
-            elif A[0] < B[len(B) / 2 - 1] and A[1] > B[len(B) / 2]:
-                return self.findMedianOfTwo(B[len(B) / 2 - 1], B[len(B) / 2])
-
-        medA = self.findMedianOfOneArray(A)
-        medB = self.findMedianOfOneArray(B)
-        if medA == medB:
-            return medA
-
-        maxDiscardable = len(A) / 2 - 1
-        if (len(A) % 2 != 0 or len(A) == 2):
-            maxDiscardable = len(A) / 2
-        if medA < medB:
-            return self.findMedianSortedArrays(A[maxDiscardable:], B[:len(B) - maxDiscardable])
-        else:
-            return self.findMedianSortedArrays(A[:len(A) - maxDiscardable], B[maxDiscardable:])
-
-A = [19,43]
-B = [0, 4, 6, 18, 39, 42]
+A = [ -40, -25, 5, 10, 14, 28, 29, 48 ]
+B = [ -48, -31, -15, -6, 1, 8 ]
 
 sol = Solution()
 print sol.findMedianSortedArrays(A, B)
