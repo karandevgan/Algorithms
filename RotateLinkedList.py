c59a060f32f0894888cc1fc5c3f202845fb6b7cc
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-07-21 12:35:15 +0530

    Rotate Linked List Without Reversal

--- RotateLinkedList.py
+++ RotateLinkedList.py
@@ -4,37 +4,23 @@ class Solution:
     # @param A : head node of linked list
     # @param B : integer
     # @return the head node in the linked list
-    def rotateRight(self, A, B):
+   def rotateRight(self, A, B):
         length = self.getLength(A)
-        if length <= 1:
+        if length == 1:
             return A
         B = B % length
         if B == 0:
             return A
-        A = self.reverseList(A, length, None, None)
-        tail = A
-        for _ in xrange(B):
-            tail = tail.next
-        A = self.reverseList(A, B, None, tail)
-        head = A
-        for _ in xrange(B-1):
-            head = head.next
-        self.reverseList(head.next, length-B, head, None)
-        return A
-
-    def reverseList(self, A, B, H, T):
-        next = T
-        while B != 0:
-            temp_next = A.next
-            A.next = next
-            next = A
-            A = temp_next
-            B -= 1
-        if H is None:
-            H = next
-        else:
-            H.next = next
-        return H
+        temp = A
+        for _ in xrange(length-B-1):
+            temp = temp.next
+        head = temp.next
+        temp.next = None
+        temp = head
+        while temp.next is not None:
+            temp = temp.next
+        temp.next = A
+        return head
     
     def getLength(self, A):
         length = 0
