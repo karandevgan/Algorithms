40d140a81034558f51a70beaa3b434d2397c8d72
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-07-21 11:06:17 +0530

    Check if list is pallindrome

--- IsListPallindrome.py
+++ IsListPallindrome.py
@@ -46,15 +46,3 @@ class Solution:
             i = i.next
             j = j.next
         return 1
-
-node1 = ListNode(1)
-# node2 = ListNode(2)
-# node3 = ListNode(3)
-# node4 = ListNode(2)
-# node5 = ListNode(2)
-# node1.next = node2
-# node2.next = node3
-# node3.next = node4
-# node4.next = node5
-sol = Solution()
-print sol.lPalin(node1)
