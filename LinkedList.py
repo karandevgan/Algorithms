480a58a63ce5f70bd9d86791318258237b32df94
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-07-26 11:08:23 +0530

    Updated to return numbers in 0 to x

--- LinkedList.py
+++ LinkedList.py
@@ -3,11 +3,11 @@ class ListNode:
         self.val = x
         self.next = None
 
-def getRandomNodes(n):
+def getRandomNodes(n, top):
     import random
     nodes = []
     for i in xrange(n):
-        node = ListNode(random.randint(1,20))
+        node = ListNode(random.randint(0,top))
         nodes.append(node)
     return nodes
 
@@ -15,13 +15,13 @@ def joinNodes(nodes):
     for i in xrange(1,len(nodes)):
         nodes[i-1].next = nodes[i]
 
-def getRandomLinkedList(n):
-    nodes = getRandomNodes(n)
+def getRandomLinkedList(n, top = 9):
+    nodes = getRandomNodes(n, top)
     joinNodes(nodes)
     return nodes[0]
 
-def getSortedLinkedList(n):
-    nodes = getRandomNodes(n)
+def getSortedLinkedList(n, top = 9):
+    nodes = getRandomNodes(n, top)
     nodes = sorted(nodes, key=lambda x: x.val)
     joinNodes(nodes)
     return nodes[0]
