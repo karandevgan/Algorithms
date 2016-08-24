ab1c55b4a13528bb9aadffb36c2647b0a94dbe0b
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-08-24 10:44:31 +0530

    Breaked into more functions

--- NQueenProblem.py
+++ NQueenProblem.py
@@ -14,47 +14,55 @@ class Solution:
         candidate_slots = [(no_of_queens_placed,j) for j in xrange(A)]
 
         for candidate in candidate_slots:
-            if candidate not in invalid_positions:
+            if self.isCandidateValid(candidate, invalid_positions):
                 new_board = board[:]
-                new_invalid_positions = set(invalid_positions)
-                position = ""
-                i = 0
-                while i != candidate[1]:
-                    position += "."
-                    i += 1
-                position += "Q"
-                while i != A-1:
-                    position += "."
-                    i += 1
-            
-                # Adds invalid points in the same col
-                for j in xrange(candidate[0]+1, A):
-                    new_invalid_positions.add((j, candidate[1]))
-                
-                # Adds invalid points on right diagonal
-                row = candidate[0]
-                col = candidate[1]
-                while row != A-1 and col != A-1:
-                    row += 1
-                    col += 1
-                    new_invalid_positions.add((row, col))
-                
-                # Adds invalid points on left diagonal
-                row = candidate[0]
-                col = candidate[1]
-                while row != A-1 and col != 0:
-                    row += 1
-                    col -= 1
-                    new_invalid_positions.add((row, col))
-                
+
+                position = self.createStringOfPositions(candidate[1], A)
                 new_board.append(position)
+
+                new_invalid_positions = set(invalid_positions)            
+                self.updateInvalidPositions(new_invalid_positions, candidate, A)
+                
                 self.auxillary(A, no_of_queens_placed+1, return_list, new_invalid_positions, new_board)
                 
                 if len(new_board) == A:
                     return_list.append(new_board)
-                
+    
+    def isCandidateValid(self, candidate, invalid_positions):
+        return candidate not in invalid_positions
         
+    def createStringOfPositions(self, queenColumn, A):
+        i = 0
+        position = []
+        while i != queenColumn:
+            position.append(".")
+            i += 1
+        position.append("Q")
+        while i != A-1:
+            position.append(".")
+            i += 1
+        return ''.join(position)
 
+    def updateInvalidPositions(self, invalid_positions, candidate, A):
+        # Adds invalid points in the same col
+        for j in xrange(candidate[0]+1, A):
+            invalid_positions.add((j, candidate[1]))
+        
+        # Adds invalid points on right diagonal
+        row = candidate[0]
+        col = candidate[1]
+        while row != A-1 and col != A-1:
+            row += 1
+            col += 1
+            invalid_positions.add((row, col))
+        
+        # Adds invalid points on left diagonal
+        row = candidate[0]
+        col = candidate[1]
+        while row != A-1 and col != 0:
+            row += 1
+            col -= 1
+            invalid_positions.add((row, col))
 
 sol = Solution()
-print sol.solveNQueens(5)
+print sol.solveNQueens(4)
