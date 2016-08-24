409ee827331d7b3ee4cb385c26d35544aa186c1e
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-08-24 11:13:20 +0530

    Removed invalid_list

--- NQueenProblem.py
+++ NQueenProblem.py
@@ -3,66 +3,50 @@ class Solution:
     # @return a list of list of strings
     def solveNQueens(self, A):
         return_list = []
-        invalid_positions = set([])
-        self.auxillary(A, 0, return_list, invalid_positions, [])
+        occupied_positions = []
+        self.auxillary(A, 0, return_list, occupied_positions, [])
         return return_list
     
-    def auxillary(self, A, no_of_queens_placed, return_list, invalid_positions, board):
+    def auxillary(self, A, no_of_queens_placed, return_list, occupied_positions, board):
         if no_of_queens_placed == A:
             return
 
         candidate_slots = [(no_of_queens_placed,j) for j in xrange(A)]
 
         for candidate in candidate_slots:
-            if self.isCandidateValid(candidate, invalid_positions):
+            if self.isCandidateValid(candidate, occupied_positions):
                 new_board = board[:]
 
                 position = self.createStringOfPositions(candidate[1], A)
                 new_board.append(position)
 
-                new_invalid_positions = set(invalid_positions)            
-                self.updateInvalidPositions(new_invalid_positions, candidate, A)
+                new_occupied_positions = occupied_positions[:]            
+                new_occupied_positions.append(candidate)
                 
-                self.auxillary(A, no_of_queens_placed+1, return_list, new_invalid_positions, new_board)
+                self.auxillary(A, no_of_queens_placed+1, return_list, new_occupied_positions, new_board)
                 
                 if len(new_board) == A:
                     return_list.append(new_board)
     
-    def isCandidateValid(self, candidate, invalid_positions):
-        return candidate not in invalid_positions
+    def isCandidateValid(self, candidate, occupied_positions):
+        for pos in occupied_positions:
+            if candidate[0] == pos[0] or candidate[1] == pos[1] or \
+                abs(candidate[0] - pos[0]) == abs(candidate[1] - pos[1]):
+                return False
+        return True
         
     def createStringOfPositions(self, queenColumn, A):
         i = 0
-        position = []
+        position = [''] * A
         while i != queenColumn:
-            position.append(".")
+            position[i] = "."
             i += 1
-        position.append("Q")
-        while i != A-1:
-            position.append(".")
+        position[i] = "Q"
+        i += 1
+        while i != A:
+            position[i] = "."
             i += 1
         return ''.join(position)
 
-    def updateInvalidPositions(self, invalid_positions, candidate, A):
-        # Adds invalid points in the same col
-        for j in xrange(candidate[0]+1, A):
-            invalid_positions.add((j, candidate[1]))
-        
-        # Adds invalid points on right diagonal
-        row = candidate[0]
-        col = candidate[1]
-        while row != A-1 and col != A-1:
-            row += 1
-            col += 1
-            invalid_positions.add((row, col))
-        
-        # Adds invalid points on left diagonal
-        row = candidate[0]
-        col = candidate[1]
-        while row != A-1 and col != 0:
-            row += 1
-            col -= 1
-            invalid_positions.add((row, col))
-
 sol = Solution()
-print sol.solveNQueens(4)
+print sol.solveNQueens(5)
